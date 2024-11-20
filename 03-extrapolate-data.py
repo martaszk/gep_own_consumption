307f9b511a86d5c98fb94f4da50440fa0d226528
Ryan McWay <ryan.mcway@gmail.com>
2024-11-20 15:40:35 -0600

    Added in 03-extrapolate step to expand data set

---
+++ 03-extrapolate-data.py
@@ -0,0 +1,13 @@
+"""
+Purpose: 
+This script uses the original own consumption and the interpolated own consumption (or comparison) to create, 
+extrapolated values for counties outside the FAO sample but meet food insecurity criteria. 
+This exports an final data set on own consumption for all qualifying countries. 
+"""
+
+# TODO: Bring in the interpolated data 
+
+# TODO: Use regression or ML to extrapolate (big task)
+
+# TODO: Export results as a data set
+
