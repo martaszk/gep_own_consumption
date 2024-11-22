4110405579bcac8e6ca973d1fed25f63fa00c961
Marta Sylla <marta.sylla@upwr.edu.pl>
2024-11-22 12:22:26 -0600

    edits to 03

--- 03-extrapolate-data.py
+++ 03-extrapolate-data.py
@@ -8,6 +8,15 @@ This exports an final data set on own consumption for all qualifying countries.
 # TODO: Bring in the interpolated data 
 
 # TODO: Use regression or ML to extrapolate (big task)
+# Run a simple OLS model
+#model = sm.OLS(Y, X).fit()
+    # TODO: We can play with the regression. 
+    # TODO: Add country FE
+    # TODO: Add time trend
+    # TODO: Add polynomial
+    # TODO: Add gdp per capita by nation by year
+    # TODO: Compare across the different regressions.
+#return model
 
 # TODO: Export results as a data set
 
